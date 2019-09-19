aeafa164871d1492f7ec84911a659f7a9af2f206
Taru Saloheimo <taru.saloheimo@gmail.com>
2019-09-19 13:20:30 +0300

    Validointi päivitetty: uuden asiakkaan rekisteröinti

--- application/asiakas/forms.py
+++ application/asiakas/forms.py
@@ -2,17 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators
 
 class AsiakasLomake(FlaskForm):
-    etunimi = StringField("etunimi", [validators.Regexp("\w", message="Etunimi ei voi koostua tyhjistä välilyönneistä"), 
+    etunimi = StringField("etunimi", [validators.Regexp("\w[^0-9]", message="Etunimi ei voi koostua tyhjistä välilyönneistä, ja etunimen tulee koostua vain kirjaimista."), 
     validators.Length(min=2, message="Etunimen pitää olla vähintään 2 kirjainta pitkä")])
     
-    sukunimi = StringField("sukunimi", [validators.Regexp("\w", message="Sukunimi ei voi koostua tyhjistä välilyönneistä"), 
+    sukunimi = StringField("sukunimi", [validators.Regexp("\w[^0-9]", message="Sukunimi ei voi koostua tyhjistä välilyönneistä, ja sukunimen tulee koostua vain kirjaimista."), 
     validators.Length(min=2, message="Sukunimen pitää olla vähintään 2 kirjainta pitkä")])
     
-    login = StringField("käyttäjätunnus", [validators.Regexp(r"^[\w.0-9]+$", message="Käyttäjätunnus ei voi koostua tyhjistä välilyönneistä. Käyttäjätunnuksessa voi olla vain kirjaimia ja numeroita."), 
-    validators.Length(min=2, message="Käyttäjätunnuksen pitää olla vähintään 2 kirjainta pitkä")])
+    login = StringField("käyttäjätunnus", [validators.Regexp("[^ ]", message="Käyttäjätunnus ei voi koostua tyhjistä välilyönneistä."), validators.Length(min=2, message="Käyttäjätunnuksen pitää olla vähintään 2 kirjainta pitkä.")])
     
-    salasana = PasswordField("salasana", [validators.Regexp(r"^[\w.0-9]+$", message="Salasana ei voi koostua tyhjistä välilyönneistä. Salasanassa voi olla vain kirjaimia ja numeroita."), 
-    validators.Length(min=2, message="Salasanan pitää olla vähintään 2 kirjainta pitkä")])
+    salasana = PasswordField("salasana", [validators.Regexp("[^ ]", message="Salasana ei voi koostua tyhjistä välilyönneistä."), 
+    validators.Length(min=2, message="Salasanan pitää olla vähintään 2 kirjainta pitkä.")])
  
     class Meta:
         csrf = False
+
+
