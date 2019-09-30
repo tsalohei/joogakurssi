202cc0595d49dc2be6f04306ad94187ccb14ce12
Taru Saloheimo <taru.saloheimo@gmail.com>
2019-09-30 20:58:08 +0300

    Lisätty validointeja

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,20 +3,19 @@ from wtforms import StringField, PasswordField, validators
 
 class KayttajaLomake(FlaskForm):
     etunimi = StringField("etunimi", [validators.Regexp("\w[^0-9]", message="Etunimi ei voi koostua tyhjistä välilyönneistä, ja etunimen tulee koostua vain kirjaimista."), 
-    validators.Length(min=2, message="Etunimen pitää olla vähintään 2 kirjainta pitkä.")])
+    validators.Length(min=2, message="Etunimen tulee olla vähintään 2 ja korkeintaan 50 kirjainta pitkä.")])
     
     sukunimi = StringField("sukunimi", [validators.Regexp("\w[^0-9]", message="Sukunimi ei voi koostua tyhjistä välilyönneistä, ja sukunimen tulee koostua vain kirjaimista."), 
-    validators.Length(min=2, message="Sukunimen pitää olla vähintään 2 kirjainta pitkä.")])
+    validators.Length(min=2, max=50, message="Sukunimen tulee olla vähintään 2 ja korkeintaan 50 kirjainta pitkä.")])
     
-    login = StringField("käyttäjätunnus", [validators.Regexp("[^ ]", message="Käyttäjätunnus ei voi koostua tyhjistä välilyönneistä."), validators.Length(min=2, message="Käyttäjätunnuksen pitää olla vähintään 2 merkkiä pitkä.")])
+    login = StringField("käyttäjätunnus", [validators.Regexp("[^ ]", message="Käyttäjätunnus ei voi koostua tyhjistä välilyönneistä."), validators.Length(min=2, max=50, message="Käyttäjätunnuksen tulee olla vähintään 2 ja korkeintaan 50 merkkiä pitkä.")])
     
     salasana = PasswordField("salasana", [validators.Regexp("[^ ]", message="Salasana ei voi koostua tyhjistä välilyönneistä."), 
-    validators.Length(min=2, message="Salasanan pitää olla vähintään 2 merkkiä pitkä.")])
+    validators.Length(min=2, max=50, message="Salasanan tulee olla vähintään 2 ja korkeintaan 50 merkkiä pitkä.")])
  
     class Meta:
         csrf = False
 
-#TÄHÄN VIELÄ VALIDOINNIT, KOPIOI YLTÄ? Ja lisäksi ettei voi olla kahta samaa käyttäjätunnusta?
 class LoginLomake(FlaskForm):
     login = StringField("käyttäjätunnus")
     salasana = PasswordField("salasana")
